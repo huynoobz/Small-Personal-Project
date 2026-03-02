360e83b543068ee5ac06007bfaec65d9d8b55515
huynoobz <quochuy0615@gmail.com>
2026-03-02 22:17:06 +0700

    u101602032026

--- android_app/talking_bill/app/src/main/java/com/example/talking_bill/AppConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.talking_bill
-
-typealias AppConfig = Map<String, AppConfigItem>
-
-data class AppConfigItem(
-    val receive_keyword: List<String>,
-    val m_regex: List<String>
-) 
